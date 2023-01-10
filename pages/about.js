a7b394292bc0e4487d79bded3037fe57082c2496
Shafayathub <shafayat.ms@gamilcom>
2023-01-10 23:31:15 +0600

    trial site is ready

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,29 @@
+import Head from 'next/head';
 import React from 'react';
 import Social from './Social';
 
 const About = () => {
   return (
-    <div className="h-screen text-green-400">
-      <h2 className="text-center text-3xl font-semibold m-10">
-        DOCTOR SERVICE BD
-      </h2>
-      <p className="text-center text-xl m-5">
-        We are committed to serve you with better Health Care. <br /> Welcome to
-        you our Doctor Service BD website to take the services and serial of all
-        expert and experienced doctors in Rajshahi.
-      </p>
-      <Social></Social>
-    </div>
+    <>
+      <Head>
+        <title>About | Doctor Service BD</title>
+        <meta name="description" content="Doctor Service BD" />
+        <meta name="keyword" content="Doctor Service BD" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-screen text-green-400">
+        <h2 className="text-center text-3xl font-semibold m-10">
+          DOCTOR SERVICE BD
+        </h2>
+        <p className="text-center text-xl m-5">
+          We are committed to serve you with better Health Care. <br /> Welcome
+          to you our Doctor Service BD website to take the services and serial
+          of all expert and experienced doctors in Rajshahi.
+        </p>
+        <Social></Social>
+      </div>
+    </>
   );
 };
 
--- pages/contact.js
+++ pages/contact.js
@@ -1,15 +1,25 @@
+import Head from 'next/head';
 import React from 'react';
 import Social from './Social';
 
 const Contact = () => {
   return (
-    <div className="h-screen">
-      <h4 className="text-center text-3xl text-green-500 font-semibold p-10">
-        Address: Ashrafunnesha Plaza, Beside of ABC School <br /> Laxmipur,
-        Rajshahi <br /> Rajshahi, Bangladesh
-      </h4>
-      <Social className="pb-10"></Social>
-    </div>
+    <>
+      <Head>
+        <title>Contact | Doctor Service BD</title>
+        <meta name="description" content="Doctor Service BD" />
+        <meta name="keyword" content="Doctor Service BD" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-screen">
+        <h4 className="text-center text-3xl text-green-500 font-semibold p-10">
+          Address: Ashrafunnesha Plaza, Beside of ABC School <br /> Laxmipur,
+          Rajshahi <br /> Rajshahi, Bangladesh
+        </h4>
+        <Social className="pb-10"></Social>
+      </div>
+    </>
   );
 };
 
